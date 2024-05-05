5c571f166d40aac71b51ae22bdd0cb89bea4290c
DGriffin91 <github@dgdigital.net>
2024-05-04 17:55:48 -0700

    include env light in minimal

--- src/main.rs
+++ src/main.rs
@@ -192,6 +192,11 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, args: Res<A
             }),
             ..default()
         },
+        EnvironmentMapLight {
+            diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
+            specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
+            intensity: 250.0,
+        },
         CameraController::default().print_controls(),
     ));
     if !args.minimal {
@@ -199,11 +204,6 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, args: Res<A
             BloomSettings {
                 intensity: 0.05,
                 ..default()
-            },
-            EnvironmentMapLight {
-                diffuse_map: asset_server.load("environment_maps/pisa_diffuse_rgb9e5_zstd.ktx2"),
-                specular_map: asset_server.load("environment_maps/pisa_specular_rgb9e5_zstd.ktx2"),
-                intensity: 250.0,
             },
             TemporalAntiAliasBundle::default(),
         ))
